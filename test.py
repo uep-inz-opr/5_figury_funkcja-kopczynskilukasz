1df3b2146d1a3bc731e26db8be96f2fdf9aa627b
LukaszK <kopczynski.l.k@gmail.com>
2022-05-21 11:47:48 +0200

    dzialajacy kod, udalo sie z maca

--- test.py
+++ test.py
@@ -1,2 +1,39 @@
 #proba kodu nr 2 
 #probna wersja z maca (cos nie działa cały czas)
+
+from cmath import sqrt
+import math
+
+def pole_figury(x):
+    if len(x) == 1:
+        wynik = math.pi*x[0]*x[0]
+        wynik = round(wynik, 2)
+    elif len(x) == 2:
+        wynik = x[0]*x[1]
+        wynik = round(wynik, 2)
+    elif len(x) == 3:
+        s = (x[0] + x[1] + x[2])/2
+        wynik = math.sqrt(s*(s - x[0]) * (s - x[1]) * (s - x[2]))
+        wynik = round(wynik, 2)
+    else:
+        return "Error!! You can put only 3 characters"
+    return wynik
+
+pole=0
+liczba_figur = input()
+liczba_figur = int(liczba_figur)
+
+for i in range(liczba_figur):
+    figura = input().strip()
+    figura = figura.split(' ')
+    for j in range(len(figura)):
+        figura[j] = float(figura[j])
+
+    wynik_funkcji = pole_figury(figura)
+    if type(wynik_funkcji) is not float:
+        pole = wynik_funkcji
+        break
+    pole = pole + wynik_funkcji
+    pole = round(pole, 2)
+
+print(pole)
